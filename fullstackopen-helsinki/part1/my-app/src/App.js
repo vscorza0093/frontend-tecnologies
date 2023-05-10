0b1df697faa2f66f2d5647a3a6d7df5c66e08963
vscorza0093 <vinicius.scorza@hotmail.com>
2023-05-10 17:17:30 -0300

    JavaScript, React, JSX studies update

--- fullstackopen-helsinki/part1/my-app/src/App.js
+++ fullstackopen-helsinki/part1/my-app/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const ReactApp = () => {
+  console.log("Função 1 -> " + ReactApp.name)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Olá, JavaScript!</h1>
+      <p>Eu sempre te odiei...</p>
+      <p>Quem sabe agora eu não mudo de ideia?</p>
+      <Crazyness />
     </div>
-  );
+  )
 }
 
-export default App;
+const Crazyness = () => {
+  console.log("Função 2 -> " + Crazyness.name)
+  return (
+    <div>
+      <h2>É bem provável</h2>
+      <p>
+        Essa coisa de JavaScript e JSX, React... loucura
+      </p>
+      <FckingCrazyness />
+    </div>
+  )
+}
+
+const FckingCrazyness = () => {
+  console.log("Função 3 -> " + FckingCrazyness.name)
+  return (
+    <div>
+      <h3>Look!</h3>
+      <p>I'm creating JSX components and calling them from other functions like html components</p>
+      <AndNow />
+    </div>
+  )
+}
+
+const AndNow = () => {
+  console.log("Função 4 -> " + AndNow.name)
+  return (
+  <div>
+    <h2>IN ENGLISH AND WITH EMOJIS &#127918;</h2>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <Finito />
+  </div>
+)}
+
+const Finito = () => {
+  console.log("Função 5 -> " + Finito.name)
+  return (
+    <div>
+      <h1>
+        Finito.
+      </h1>
+    </div>
+)}
+
+export default ReactApp
